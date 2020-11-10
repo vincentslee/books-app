18006df33b145f036f4e2fa8d3a02055911872b4
Vincent Lee <911vincentlee@gmail.com>
2020-11-10 14:04:39 -0600

    Updates

---
+++ precache-manifest.b231fa3d55b4e5dab1c2441c7f6b50dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2940369d24d78f0f87f",
+    "url": "/books-app/static/css/main.bd3f3d18.chunk.css"
+  },
+  {
+    "revision": "e2940369d24d78f0f87f",
+    "url": "/books-app/static/js/main.a08ff761.chunk.js"
+  },
+  {
+    "revision": "bb01db2d8de36c95f5da",
+    "url": "/books-app/static/js/runtime~main.c654da95.js"
+  },
+  {
+    "revision": "abfaa5ccb473e7a5d12f",
+    "url": "/books-app/static/js/2.22513f47.chunk.js"
+  },
+  {
+    "revision": "315142b7321373e4cb89d2777811af79",
+    "url": "/books-app/index.html"
+  }
+];
